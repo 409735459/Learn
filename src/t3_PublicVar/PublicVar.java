1d4793a2c4a374aea249507afefd372034251054
dawei16 <dawei16@staff.weibo.com>
2017-12-20 19:10:03 +0800

    脏读例子

---
+++ src/t3_PublicVar/PublicVar.java
@@ -0,0 +1,20 @@
+package t3_PublicVar;
+
+public class PublicVar {
+    private String username = "A";
+    private String password = "AA";
+    public synchronized void setValue(String username,String password) {
+        try{
+            this.username = username;
+            Thread.sleep(5000);
+            this.password = password;
+            System.out.println("setValue method thread name="+Thread.currentThread().getName()+" username="+username+" password="+password);
+        } catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public synchronized void getValue() {
+        System.out.println("getValue method thread name="+Thread.currentThread().getName()+" username="+username+" password="+password);
+    }
+}
