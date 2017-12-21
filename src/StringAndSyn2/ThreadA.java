310a2d28a68cce9649421ff08c772dcfe4aaf310
dawei16 <dawei16@staff.weibo.com>
2017-12-21 19:19:37 +0800

    add

---
+++ src/StringAndSyn2/ThreadA.java
@@ -0,0 +1,15 @@
+package StringAndSyn2;
+
+public class ThreadA extends Thread{
+    private Service service;
+
+    public ThreadA(Service service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        service.print(new Object());
+    }
+}
